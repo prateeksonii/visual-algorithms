6a106e0d466e587fa4ff58b89c99683f5e48f8cd
Prateek Soni <prateek_soni@outlook.com>
2022-08-13 02:04:19 +0530

    update nav to show active link
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Nav = () => {
+  const router = useRouter();
+
   return (
     <nav className="h-16 bg-zinc-800">
       <div className="container mx-auto flex h-full items-center">
@@ -11,9 +14,28 @@ const Nav = () => {
             </a>
           </Link>
         </div>
-        <div className="ml-12 flex-1 text-zinc-300 hover:text-zinc-200">
+        <div className="ml-12 flex flex-1 items-center gap-8 text-zinc-300 hover:text-zinc-200">
           <Link href="/explore">
-            <a className="text-lg">Explore</a>
+            <a
+              className={`text-lg ${
+                router.asPath.includes("explore")
+                  ? "font-bold text-emerald-400"
+                  : ""
+              }`}
+            >
+              Explore
+            </a>
+          </Link>
+          <Link href="/play">
+            <a
+              className={`text-lg ${
+                router.asPath.includes("play")
+                  ? "font-bold text-emerald-400"
+                  : ""
+              }`}
+            >
+              Play
+            </a>
           </Link>
         </div>
       </div>
---
+++ src/pages/explore.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Nav from "../components/Nav";
+
+const ExplorePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>VA | Explore</title>
+      </Head>
+      <Nav />
+      <main>ok</main>
+    </>
+  );
+};
+
+export default ExplorePage;
